6e94ce43713f9b90336fae4e172d48776628f02f
yusanguang <tangtongguang@sina.com>
2020-12-28 13:44:54 +0800

    add swagger

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,28 @@
 import express from 'express'
 const router = express.Router();
 import { genUsers } from '../schema/users';
+
+
+
+/**
+  * @swagger
+  * /users:
+  *   get:
+  *     description: Returns users
+  *     tags:
+  *      - Users
+  *     produces:
+  *      - application/json
+  *     responses:
+  *       200:
+  *         description: users
+  */
 router.get('/echo', async (req, res) => {
   const data = await genUsers(5);
   res.jsonp({
     data
   })
 })
+
+
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,59 @@ import { green } from 'chalk';
 import jsonServer from 'json-server';
 import path from 'path';
 import addRoute from './router'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const PORT = 3000;
+
+const swaggerDefinition = {
+  info: {
+    // API informations (required)
+    title: 'Hello World', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'A sample API', // Description (optional)
+  },
+  host: `localhost:${PORT}`, // Host (optional)
+  basePath: '/', // Base path (optional)
+};
+
+
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
+  apis: [path.join(__dirname, './controllers/*.ts'),
+    //'./examples/app/parameters.yaml'
+  ],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJsdoc(options);
+
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
-
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
 addRoute(server)
+
+// Serve swagger docs the way you like (Recommendation: swagger-tools)
+server.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 // Add custom routes before JSON Server router
 
 server.use(router)
-server.listen(3000, () => {
+
+
+server.listen(PORT, () => {
   console.log(green('JSON server is running'))
 })
